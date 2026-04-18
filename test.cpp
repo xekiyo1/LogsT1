844b30466e22e4a201554d90ce28160deb260e28
Javier Azócar <ignacio.azocar.carcamo@gmail.com>
2026-04-17 20:37:25 -0400

    Test de leer archivo

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+
+    // Open the binary file for reading
+    ifstream file("europa.bin", ios::binary);
+    if (!file) {
+        cerr << "Error opening file for reading.";
+        return 1;
+    }
+
+    float hola[2];
+    int i=0;
+    while (file.read((char*)hola, sizeof(float)*2)) {
+    	cout<<"punto "<<++i<<": "<<hola[0]<<' '<<hola[1]<<endl;
+    }
+    
+    // Close file
+    file.close();
+
+    printf("terminé con %d bloques",i);
+    return 0;
+}
