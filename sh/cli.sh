93656e39b8dfc838b144ce4eb3340689388db9c4
Hernan Poblete <hernan.poblete@ug.uchile.cl>
2026-05-03 10:23:02 -0400

    chore(sh/cli): Re-Adds looping

--- sh/cli.sh
+++ sh/cli.sh
@@ -14,16 +14,16 @@ main_loop(){
 			no_ext=${i/.out}
 			echo ">	${no_ext:6}"
 		done
-		
-
+	elif [ "$command" == "exit" ]; then
+		echo "Bye-Bye"
 	elif [ -f bin/${stringarr[0]}.out ]; then
 		bin/${stringarr[0]}.out ${stringarr[@]:1}
 	else
 		echo "El comando ${stringarr[0]} no existe. ¿Compilaste los archivos?"
 	fi
 
-	if false; then
-	#if [ "$command" != "exit" ]; then
+	#if false; then
+	if [ "$command" != "exit" ]; then
 		main_loop
 	fi
 }
