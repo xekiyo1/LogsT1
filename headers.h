7de538b6d1005dfa537f6c0a261b0c28d904ddff
ElNChou <ignacio.azocar.carcamo@gmail.com>
2026-05-03 12:42:01 -0400

    Fix: Declaration in Headers

--- headers.h
+++ headers.h
@@ -5,7 +5,4 @@ using namespace std;
 
 #include "./RTreeConstructors/RTree.h"
 
-NearestXConstructor NearestX;
-STRConstructor STR;
-
 #endif
--- test.cpp
+++ test.cpp
@@ -1,6 +1,10 @@
 #include "headers.h"
 #include <bits/stdc++.h>
 using namespace std;
+
+NearestXConstructor NearestX;
+STRConstructor STR;
+
 int main() {
     NearestX.CreateRTree("europa.bin","arbolitoNearestX.bin");
     STR.CreateRTree("europa.bin","arbolitoSTR.bin");
